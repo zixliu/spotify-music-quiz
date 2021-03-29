6c69995854ea8233f418bfe772bc8dae59486930
Zixuan Liu <zixuan.liu@entur.org>
2021-03-30 01:02:56 +0200

    Add authentication logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <h1>Hello</h1>
+    //   </header>
+    // </div>
+    // {!token && <Login />}
+      // {token && <Player spotify={s} />}
+    <div className="app">
+      <Login />
     </div>
   );
 }
